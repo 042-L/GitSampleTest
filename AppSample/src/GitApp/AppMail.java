ea736b12b65af0bf65543540a227d7599429d6c0
042-L <24k.mobile@gmail.com>
2021-11-30 15:19:24 +0800

    3rd Version

--- AppSample/src/GitApp/AppMail.java
+++ AppSample/src/GitApp/AppMail.java
@@ -4,7 +4,7 @@ public class AppMail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2nd Version");
+		System.out.println("3nd Version by Team");
 	}
 
 }
