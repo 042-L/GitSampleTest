55f47d61011973795cbb24ffb7859218cc7fd0d1
042-L <24k.mobile@gmail.com>
2021-11-30 15:34:21 +0800

    Add MathClass

--- AppSample/src/GitApp/AppMail.java
+++ AppSample/src/GitApp/AppMail.java
@@ -4,7 +4,14 @@ public class AppMail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("3nd Version by Team");
+		System.out.println("4nd Version by Team");
+		Math cMath = new Math();
+		System.out.println(cMath.add(10, 5));
 	}
 
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
